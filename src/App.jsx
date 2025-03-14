2a3d5aa75b126255958fcba54cc36ea4e08efae6
Nilou <desnillight@gmail.com>
2025-03-14 17:58:05 +0330

    styling the mobile nav

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,267 @@ export default function App() {
   return (
     <>
       <Header />
+
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
+      <div className="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+        voluptatibus eum, atque velit delectus veniam ab eos ullam dolorem
+        debitis dicta adipisci nemo ut dolor ex quis praesentium quisquam fugit
+        ea porro! Nobis deserunt distinctio asperiores. Aut tempore laudantium
+        sed autem, quod repudiandae alias ex labore tenetur voluptates maiores
+        quo accusantium, vel facere consequatur minus! Expedita voluptatibus
+        ipsum nihil officia fugit reprehenderit quibusdam ratione molestias
+        explicabo, ipsa fugiat doloremque odio aliquid recusandae eius tempora
+        deserunt perferendis excepturi quis blanditiis optio maxime dolore.
+        Totam consequuntur maiores, praesentium eaque minima dolore est illum
+        sequi odit unde vel qui? Est, illo! Suscipit, quas?
+      </div>
     </>
   );
 }
--- src/Header/header.jsx
+++ src/Header/header.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSkull } from "@fortawesome/free-solid-svg-icons";
 export default function Header() {
   return (
-    <header className="fixed top-4 w-[96%] left-0 right-0 mx-auto  h-15 rounded-2xl backdrop-blur-[2px]">
+    <header className="fixed top-4 w-[96%] left-0 right-0 mx-auto  h-15 rounded-2xl backdrop-blur-md">
       <div className="container  h-full flex justify-between items-center">
         <div>
           <FontAwesomeIcon
