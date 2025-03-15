22de3626a117279726d7eca2442d99aa1544f9aa
Nilou <desnillight@gmail.com>
2025-03-15 20:05:29 +0330

    adding the shin animation

---
+++ src/TextAnimations/ShinyText/ShinyText.jsx
@@ -0,0 +1,21 @@
+const ShinyText = ({ text, speed = 5, className = "" }) => {
+  const animationDuration = `${speed}s`;
+
+  return (
+    <div
+      className={`text-[#b5b5b5a4] bg-clip-text inline-block animate-shine
+      } ${className}`}
+      style={{
+        backgroundImage:
+          "linear-gradient(120deg, rgba(255, 255, 255, 0) 40%, rgba(255, 255, 255, 0.8) 50%, rgba(255, 255, 255, 0) 60%)",
+        backgroundSize: "200% 100%",
+        WebkitBackgroundClip: "text",
+        animationDuration: animationDuration,
+      }}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default ShinyText;
