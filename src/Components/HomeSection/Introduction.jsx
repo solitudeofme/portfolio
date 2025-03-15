101eb64c1d7a3883314b964a305cdc26441f1c28
Nilou <desnillight@gmail.com>
2025-03-15 14:12:22 +0330

    adding introduction to home section

---
+++ src/Components/HomeSection/Introduction.jsx
@@ -0,0 +1,18 @@
+export default function Introdunction() {
+  return (
+    <div className="absolute space-y-3 top-30 left-1/5 sm:top-1/4 sm:left-[8%] w-[75%] sm:w-1/2 lg:w-1/3">
+      <h2 className="text-primarycolor text-2xl sm:text-4xl font-montserrat-black">
+        Hi I’m Niloufar.
+      </h2>
+      <h1 className="text-xl sm:text-3xl text-white font-montserrat-bold">
+        FRONT-END DEVELOPER
+      </h1>
+      <p className="text-white font-onest-semibold text-lg sm:text-2xl">
+        Obsessed with details, always looking for the best approach—because
+        “good enough” isn’t in my vocabulary. Deep into React, but I keep an eye
+        on other front-end technologies too. Oh, and UI/UX? Let’s just say I
+        won’t let my work dress badly.
+      </p>
+    </div>
+  );
+}
