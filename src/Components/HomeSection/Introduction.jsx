a06f49a9a63fe4a1495d23e1e7c0bc30b11a9986
Nilou <desnillight@gmail.com>
2025-03-15 20:04:57 +0330

    adding the typewiriter effect on title

--- src/Components/HomeSection/Introduction.jsx
+++ src/Components/HomeSection/Introduction.jsx
@@ -1,11 +1,21 @@
+import { Typewriter } from "react-simple-typewriter";
 export default function Introdunction() {
   return (
-    <div className="absolute space-y-3 top-30 left-1/5 sm:top-1/4 sm:left-[8%] w-[75%] sm:w-1/2 lg:w-1/3">
-      <h2 className="text-primarycolor text-2xl sm:text-4xl font-montserrat-black">
+    <div className="absolute space-y-3z top-30 left-1/5 sm:top-1/4 sm:left-[8%] w-[75%] sm:w-1/2 lg:w-1/3">
+      <h2 className="text-primarycolor text-3xl sm:text-5xl font-montserrat-black">
         Hi I’m Niloufar.
       </h2>
-      <h1 className="text-xl sm:text-3xl text-white font-montserrat-bold">
-        FRONT-END DEVELOPER
+      <h1 className="text-xl sm:text-3xl text-white font-montserrat-bold uppercase">
+        <Typewriter
+          words={[
+            "Front-end developer",
+            "React Enthusaist",
+            "Javascript Wizard",
+            "css artist",
+          ]}
+          cursor={true}
+          loop={true}
+        />
       </h1>
       <p className="text-white font-onest-semibold text-lg sm:text-2xl">
         Obsessed with details, always looking for the best approach—because
