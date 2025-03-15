35949cd755911de80b1143c8653ac9e377a660f7
Nilou <desnillight@gmail.com>
2025-03-15 20:06:39 +0330

    adding better white space to home section introduction

--- src/Components/HomeSection/Introduction.jsx
+++ src/Components/HomeSection/Introduction.jsx
@@ -1,7 +1,7 @@
 import { Typewriter } from "react-simple-typewriter";
 export default function Introdunction() {
   return (
-    <div className="absolute space-y-3z top-30 left-1/5 sm:top-1/4 sm:left-[8%] w-[75%] sm:w-1/2 lg:w-1/3">
+    <div className="absolute space-y-3 top-30 left-1/5 sm:top-1/4 sm:left-[8%] w-[75%] sm:w-1/2 lg:w-1/3">
       <h2 className="text-primarycolor text-3xl sm:text-5xl font-montserrat-black">
         Hi I’m Niloufar.
       </h2>
@@ -17,7 +17,7 @@ export default function Introdunction() {
           loop={true}
         />
       </h1>
-      <p className="text-white font-onest-semibold text-lg sm:text-2xl">
+      <p className="text-white font-onest-semibold text-lg sm:text-2xl mt-5 sm:mt-10">
         Obsessed with details, always looking for the best approach—because
         “good enough” isn’t in my vocabulary. Deep into React, but I keep an eye
         on other front-end technologies too. Oh, and UI/UX? Let’s just say I
